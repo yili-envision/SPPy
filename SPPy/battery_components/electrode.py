b10d6d5f09b7f1bfc097fd6b12da683e858d734b
m0in92 <moinahmed100@gmail.com>
2023-06-08 22:16:56 -0400

    refactored electrode

--- SPPy/battery_components/electrode.py
+++ SPPy/battery_components/electrode.py
@@ -1,4 +1,7 @@
+from typing import Optional
+from dataclasses import dataclass, field
 import collections
+
 import numpy as np
 import pandas as pd
 
@@ -6,13 +9,36 @@ from SPPy.calc_helpers import constants
 from SPPy.warnings_and_exceptions import custom_exceptions
 
 
-class Electrode:
+@dataclass
+class Electrode_:
     """
     This class is used to create an electrode object. This object is used to store the electrode parameters and provide
     relevant electrode methods. It takes input parameters from the csv file. The electrode parameters are stored as
     attributes of the object.
     """
-    def __init__(self, file_path, SOC_init, T, func_OCP, func_dOCPdT):
+    L: float # Electrode Thickness [m]
+    A: float # Electrode Area [m^2]
+    kappa: float # Ionic Conductivity [S m^-1]
+    epsilon: float # Volume Fraction
+    max_conc: float # Max. Conc. [mol m^-3]
+    R: float # Radius [m]
+    S: Optional[float] # Electroactive Area [m^2]
+    T_ref: float # Reference Temperature [K]
+    D_ref: float # Reference Diffusitivity [m^2 s^-1]
+    k_ref: float # Reference Rate Constant [m^2.5 mol^-0.5 s^-1]
+    Ea_D: float # Activation Energy of Diffusion [J mol^-1]
+    Ea_R: float # Activation Energy of Reaction [J mol^-1]
+    alpha: float # Anodic Transfer Coefficient
+    brugg: float # Bruggerman Coefficient
+    SOC_init: float # intial SOC
+    func_OCP: collections.abc.Callable[[float], [float]] # electrode open-circuit potential function that takes SOC as
+    # its arguments
+    func_dOCPdT: collections.abc.Callable[[float], [float]] # the function that represents the change of open-curcuit
+    # potential with SOC
+    T: float # electrode temperature, K
+    electrode_type: str = field(default='none')  # electrode type: none, 'p', or 'n'
+
+    def __post_init__(self):
         """
         Electrode class constructor
         :param file_path: file path of the csv containing the electrode parameters. Please adhere to the csv format
@@ -22,52 +48,23 @@ class Electrode:
         :param func_OCP: function that describes the OCP of the electrode.
         :param func_dOCPdT: a function that describes the change of OCP with temperature
         """
-        # Read and parse the csv file.
-        df = Electrode.parse_csv(file_path=file_path)
-        # Relevant instance variables are assigned.
-        self.L = df['Electrode Thickness [m]']
-        self.A = df['Electrode Area [m^2]']
-        self.kappa = df['Ionic Conductivity [S m^-1]']
-        self.epsilon = df['Volume Fraction']
-        self.max_conc = df['Max. Conc. [mol m^-3]']
-        self.R = df['Radius [m]']
-        self.S = df['Electroactive Area [m^2]']
         if np.isnan(self.S):
             self.S = 3 * self.epsilon * (self.A * self.L) / self.R
-        self.T_ref = df['Reference Temperature [K]']
-        self.D_ref = df['Reference Diffusitivity [m^2 s^-1]']
-        self.k_ref = df['Reference Rate Constant [m^2.5 mol^-0.5 s^-1]']
-        self.Ea_D = df['Activation Energy of Diffusion [J mol^-1]']
-        self.Ea_R = df['Activation Energy of Reaction [J mol^-1]']
-        self.alpha = df['Anodic Transfer Coefficient']
-        self.brugg = df['Bruggerman Coefficient']
         self.kappa_eff = self.kappa * (self.epsilon ** self.brugg)
-        self.electrode_type = "none"
-        # Set current SOC and temperature
+
         # Check if SOC is within the threshold
-        if (SOC_init<=0) or (SOC_init>=1):
+        if (self.SOC_init <= 0) or (self.SOC_init >= 1):
             raise custom_exceptions.InvalidSOCException(self.electrode_type)
-        self.SOC_ = SOC_init
-        self.T = T
-        # set OCP and dOCPdT functions
+        self.SOC_ = self.SOC_init
+
         # Check if inputted func_OCP is a function type.
-        if not isinstance(func_OCP, collections.abc.Callable):
+        if not isinstance(self.func_OCP, collections.abc.Callable):
             raise TypeError("func_OCP argument needs to be of a function type.")
-        self.func_OCP = func_OCP
         # Check if inputted func_dOCPdt is a function type.
-        if isinstance(func_dOCPdT, collections.abc.Callable):
-            self.func_dOCPdT = func_dOCPdT
+        if isinstance(self.func_dOCPdT, collections.abc.Callable):
+            self.func_dOCPdT = self.func_dOCPdT
         else:
             raise TypeError("func_dOCPdT needs to be a None or Function type")
-
-    @staticmethod
-    def parse_csv(file_path):
-        """
-        reads the csv file and returns a Pandas DataFrame.
-        :param file_path: the absolute or relative file drectory of the csv file.
-        :return: the dataframe with the column containing numerical values only.
-        """
-        return pd.read_csv(file_path, index_col=0)["Value"]
 
     @property
     def SOC(self):
@@ -97,6 +94,50 @@ class Electrode:
 
     def i_0(self, c_e):
         return self.k * self.max_conc * (c_e ** 0.5) * ((1 - self.SOC)**0.5) * (self.SOC ** 0.5)
+
+
+class Electrode(Electrode_):
+    """
+    Electrode inherits from Electrode, and it reads most of its class attributes from the csv file.
+    """
+    def __init__(self, file_path, SOC_init, T, func_OCP, func_dOCPdT):
+        """
+        Electrode class constructor
+        :param file_path: file path of the csv containing the electrode parameters. Please adhere to the csv format
+        in the data/param_pos-electrode.csv or data/param_neg-electrode.csv file.
+        :param SOC_init: state of charge of the electrode and is between 0 and 1.
+        :param T: current electrode temperature.
+        :param func_OCP: function that describes the OCP of the electrode.
+        :param func_dOCPdT: a function that describes the change of OCP with temperature
+        """
+        df = Electrode.parse_csv(file_path=file_path) # Read and parse the csv file.
+        L = df['Electrode Thickness [m]']
+        A = df['Electrode Area [m^2]']
+        kappa = df['Ionic Conductivity [S m^-1]']
+        epsilon = df['Volume Fraction']
+        max_conc = df['Max. Conc. [mol m^-3]']
+        R = df['Radius [m]']
+        S = df['Electroactive Area [m^2]']
+        T_ref = df['Reference Temperature [K]']
+        D_ref = df['Reference Diffusitivity [m^2 s^-1]']
+        k_ref = df['Reference Rate Constant [m^2.5 mol^-0.5 s^-1]']
+        Ea_D = df['Activation Energy of Diffusion [J mol^-1]']
+        Ea_R = df['Activation Energy of Reaction [J mol^-1]']
+        alpha = df['Anodic Transfer Coefficient']
+        brugg = df['Bruggerman Coefficient']
+
+        super().__init__(L=L, A=A, kappa=kappa, epsilon=epsilon, max_conc=max_conc, R=R, S=S, T_ref=T_ref, D_ref=D_ref,
+                         k_ref=k_ref, Ea_D=Ea_D, Ea_R=Ea_R, alpha=alpha, brugg=brugg, SOC_init=SOC_init,
+                         func_OCP=func_OCP, func_dOCPdT=func_dOCPdT, T=T)
+
+    @staticmethod
+    def parse_csv(file_path):
+        """
+        reads the csv file and returns a Pandas DataFrame.
+        :param file_path: the absolute or relative file drectory of the csv file.
+        :return: the dataframe with the column containing numerical values only.
+        """
+        return pd.read_csv(file_path, index_col=0)["Value"]
 
 
 class PElectrode(Electrode):
